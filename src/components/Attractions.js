e7a85d632012c314c69e35d1fb538200468e8f65
Karolina <47788618+karolinakowalczyk@users.noreply.github.com>
2021-09-14 22:23:59 +0200

    error catch when data don't load

--- src/components/Attractions.js
+++ src/components/Attractions.js
@@ -2,33 +2,40 @@ import React, { useState, useEffect } from "react";
 import AttractionsService from "../services/attractions.service";
 import createUUID from "../helpers/createUUID";
 import { Button } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
 
 const Attractions = (props) => {
   const [attractionsData, setAttractionsData] = useState([]);
+  const [successful, setSuccessful] = useState(true);
+  const [message, setMessage] = useState("");
+
   useEffect(() => {
     AttractionsService.getAttractions().then(
       (response) => {
         setAttractionsData(response.data);
       },
       (error) => {
-        const _content =
+        const resMessage =
           (error.response &&
             error.response.data &&
             error.response.data.message) ||
           error.message ||
           error.toString();
-
-        setAttractionsData(_content);
+          setMessage(resMessage);
+          setSuccessful(false); 
+        
+        //setAttractionsData(_content);
       }
     );
   }, []);
 
-const convertMinsToTime = (mins) => {
-  let hours = Math.floor(mins / 60);
-  let minutes = mins % 60;
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  return `${hours ? `${hours}:` : ''}${minutes}`;
-}
+  const convertMinsToTime = (mins) => {
+    let hours = Math.floor(mins / 60);
+    let minutes = mins % 60;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    return `${hours ? `${hours}:` : ''}${minutes}`;
+  }
+
 const displayAttractions = attractionsData.map((attraction, index) =>
   <div key={index}>
     <li key={attraction.name}>{attraction.name}</li>
@@ -44,6 +51,11 @@ const displayAttractions = attractionsData.map((attraction, index) =>
 
   return (
     <div>
+      {!successful && (
+        <div>
+            <Alert severity="error" >{message}</Alert>
+        </div>
+       )}
       <h2>attractions</h2>
       <ul>
         {displayAttractions}
