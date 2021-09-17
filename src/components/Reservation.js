358ab34754c00d2d6a6801653cf25bb8bd5414de
Karolina <47788618+karolinakowalczyk@users.noreply.github.com>
2021-09-17 16:16:38 +0200

    small changes

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,38 +1,34 @@
 import React, { useState } from "react";
 //import AuthService from "../services/auth.service";
-import { Box, Button } from '@material-ui/core';
+import { Box, Button, Link } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Calendar from 'react-calendar';
 import ReservationsService from "../services/reservations.service";
 import AuthService from "../services/auth.service";
+import Alert from '@material-ui/lab/Alert';
 
 
 const useStyles = makeStyles((theme) => ({
-  mainBox: {
-    backgroundColor: theme.palette.primary.main,
-    paddingTop: theme.spacing(3),
-    display:"flex",
-    justifyContent:"center"
-  },
-  childBox: {
-    width: '50%',
-    "@media (max-width: 48rem)": {
-      width: '100%',
+  submit: {
+    marginTop: theme.spacing(1),
+    width: "101.25%",
+    '&:hover': {
+      background: "#777777",
     },
-  },
-  input: {
-    width: '50%',
-    height: '2rem',
-  },
-  title: {
-    marginBottom: '2rem',
   },
   alert: {
     marginTop: theme.spacing(1),
-  },
+    },
+    link: {
+        '&:hover': {
+            textDecoration: 'none',
+        }
+    }
+  
 }));
 
 const Reservation = () => {
+    const classes = useStyles();
     const currentUser = AuthService.getCurrentUser();
     const days = 2;
 
@@ -42,14 +38,7 @@ const Reservation = () => {
         return current;
     } 
 
-    
-    //const classes = useStyles();
-    //const currentUser = AuthService.getCurrentUser();
     const [date, setChangeDate] = useState(new Date());
-    //const [addedDays, setAddedDays] = useState((new Date().getDate() + 2).toString());
-    ///const [addedDate, setAddedDate] = useState((new Date().setDate(new Date().getDate() + days)).toDateString());
-    //const [expiredDate, setExpiredDate] = useState((new Date(date)).setDate((new Date(date)).getDate() + 2));
-    //const [expiredDate, setExpiredDate] = useState(new Date(date));
     const [expiredDate, setExpiredDate] = useState(initalizeExpiredDate(date));
     const [currentDay, setCurrentDay] = useState(new Date().getDate().toString());
     const [currentMonth, setCurrentMonth] = useState((new Date().getMonth()).toString());
@@ -64,16 +53,9 @@ const Reservation = () => {
         setCurrentDay((date.getDate()).toString());
         setCurrentMonth((date.getMonth()).toString());
         setCurrentYear(date.getFullYear().toString());
-        //setAddedDays((date.getDate() + 2).toString());
         const current = new Date(date);
         current.setDate(current.getDate() + days);
-        //console.log("EXPIRED DATE" + current.toDateString());
         setExpiredDate(current.toDateString());
-        
-        //const current = new Date();
-        //current.setDate(current.getDate() + 2);
-        //console.log("to" + current.toDateString());
-
     }
 
     const getMonthName = (month) => {
@@ -112,7 +94,6 @@ const Reservation = () => {
         setMessage("");
         setLoading(true);
         setSuccessful(false);
-
     
         ReservationsService.createReservation(currentUser.id, date.toString(), expiredDate.toString()).then(
             (response) => {
@@ -153,6 +134,26 @@ const Reservation = () => {
             <p>{date.toString()}</p>
             <p>{expiredDate.toString()}</p>
             <Button onClick={handleMakeReservation}>Make reservation</Button>
+            {message && successful && (
+                <div className={classes.alert}>
+                <Alert severity="success" >{message}</Alert>
+                <Button
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}>
+                    <Link className={classes.link} href={"/login"} variant="body2">
+                    {"Login"}
+                    </Link>
+                </Button>
+                
+                </div>
+            )}
+            {message && !successful && (
+                <div className={classes.alert}>
+                <Alert severity="error" >{message}</Alert>
+                </div>
+            )}
         </Box>
     );
 };
