5c4d0fffd8c727fdb1f83c757f3f5b482cb76003
Karolina <47788618+karolinakowalczyk@users.noreply.github.com>
2021-10-02 00:39:42 +0200

    styling help animals section

--- src/components/AnimalHelper.js
+++ src/components/AnimalHelper.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { LoadScript } from '@react-google-maps/api';
 import StoreMap from './StoreMap';
 import { Card, Grid, CardMedia, Typography, Link, Button } from '@mui/material/';
+import shelterDog from '../assets/images/shlelterDog.jpg';
 
 const lib = ['places', 'geometry'];
 const key = `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
@@ -14,27 +15,29 @@ const AnimalHelper = ()  => {
     container
     spacing={4}
     justifyContent="center"
-    //justify="center"
-    //alignItems="center"
-    //justifyItems="center"
     >
       <Grid item xs={12} sm={6}>
         <Card sx={{ marginLeft: "1rem", marginTop: "1rem" }}>
-          <Typography gutterBottom variant="h5" component="div" sx={{ marginLeft: "0.5rem"}}>
-            Check where the nearest pet store is: 
-          </Typography>
           <CardMedia style={{ display:'flex', justifyContent:'center' }}>
           <LoadScript googleMapsApiKey={key} libraries={lib} sensor={sensor}>
             <StoreMap />
           </LoadScript>
           </CardMedia>
           <Typography gutterBottom variant="h5" component="div" sx={{ marginLeft: "0.5rem"}}>
+            Check where the nearest pet store is. 
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{ marginLeft: "0.5rem"}}>
             You can buy there food, blankets or toys and take it to the nearest shelter.
           </Typography>
         </Card>
       </Grid>
       <Grid item xs={12} sm={6}>
-        <Card sx={{ marginRight: "1rem", marginTop: "1rem" }}>
+        <Card sx={{ marginRight: "1rem", marginTop: "1rem" }}><CardMedia
+          component="img"
+          height="500"
+          src={shelterDog}
+          alt="pet shelter dog"
+        />
           <Typography gutterBottom variant="h5" component="div" sx={{ marginLeft: "0.5rem"}}>
             Do you have a blanket that you don't need? 
           </Typography>
